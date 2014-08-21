2f84bf79d231c71699ef458b2c1a48184fc4cf97
Pedro R. Jiménez <countzyx@gmail.com>
2014-08-21 01:11:06 -0400

    Pro ASP.Net MVC 5 - Ch 06 - Created a Chain of Dependency
    Refactored the code to create a new interface, IDiscountHelper, to process
    the discounts. It is now a dependency for LinqValueCalculator, which Ninject
    will resolve automatically based on the binding rules.

--- Chapter06/EssentialTools/Models/LinqValueCalculator.cs
+++ Chapter06/EssentialTools/Models/LinqValueCalculator.cs
@@ -3,6 +3,12 @@ using System.Linq;
 
 namespace EssentialTools.Models {
     public class LinqValueCalculator : IValueCalculator {
+        private IDiscountHelper discounter;
+
+        public LinqValueCalculator(IDiscountHelper discountParam) {
+            discounter = discountParam;
+        }
+
         public decimal ValueProducts(IEnumerable<Product> products) {
             return products.Sum(prod => prod.Price);
         }
