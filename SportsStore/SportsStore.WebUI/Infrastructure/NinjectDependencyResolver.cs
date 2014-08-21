87251c7259022ef8aee368b66f7e06b6dc05abf7
Pedro R. Jiménez <countzyx@gmail.com>
2014-08-21 06:26:19 -0400

    Pro ASP.Net MVC 5 - SportsStore - Ch 07 - Switched to a SQL Server LocalDB
    Replaced the mock repository with an Entity Framework on using LocalDB.

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -7,6 +7,7 @@ using Moq;
 using Ninject;
 
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure {
@@ -29,15 +30,7 @@ namespace SportsStore.WebUI.Infrastructure {
         }
 
         private void AddBindings() {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf Board", Price = 179 },
-                new Product { Name = "Running Shoes", Price = 95}
-            });
-
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
