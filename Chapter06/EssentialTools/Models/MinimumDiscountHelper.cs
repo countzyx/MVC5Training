6891563deefc6fba99282a034bac9313b359cf2f
Pedro R. Jiménez <countzyx@gmail.com>
2014-08-21 03:34:06 -0400

    Pro ASP.Net MVC 5 - Ch 06 - Started Unit Testing
    Created MinimumDiscountHelper in order to test it out. Created a new project,
    EssentialTools.Tests, in order to start unit testing. The test will fail since
    MinimumDiscountHelper is not really implemented yet.

---
+++ Chapter06/EssentialTools/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EssentialTools.Models {
+    public class MinimumDiscountHelper : IDiscountHelper {
+        public decimal ApplyDiscount(decimal totalParam) {
+            throw new NotImplementedException();
+        }
+    }
+}
