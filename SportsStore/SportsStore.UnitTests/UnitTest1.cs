0d7565df683826266f17f386e14fa541687bc8ea
Pedro R. Jiménez <countzyx@gmail.com>
2014-08-21 17:46:04 -0400

    Pro ASP.Net MVC 5 - SportsStore - Ch 07 - Continuing Pagination Support
    Added a viewmodel and a helper, with some unit testing.

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
 
 using Moq;
 
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.HtmlHelpers;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests {
     [TestClass]
@@ -33,6 +37,27 @@ namespace SportsStore.UnitTests {
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        
+        [TestMethod]
+        public void Can_Generate_Page_Links() {
+            // arrange
+            HtmlHelper myHelper = null;
+            var pagingInfo = new PagingInfo {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // act
+            var result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Page3"">3</a>", result.ToString());
         }
     }
 }
