5003a2cd4708fb287145a2812833ca0c3d01f496
Pedro R. Jiménez <countzyx@gmail.com>
2014-08-21 01:41:22 -0400

    Pro ASP.Net MVC 5 - Ch 06 - Specified Constructor Arguments With Ninject
    Refactored the code to have Ninject set the discount size via the AddBindings
    method in the NinjectDependencyResolver through a constructor on
    DefaultDiscountHelper.

--- Chapter06/EssentialTools/Models/DefaultDiscountHelper.cs
+++ Chapter06/EssentialTools/Models/DefaultDiscountHelper.cs
@@ -5,10 +5,14 @@ using System.Web;
 
 namespace EssentialTools.Models {
     public class DefaultDiscountHelper : IDiscountHelper {
-        public decimal DiscountSize { get; set; }
+        private decimal discountSize;
+
+        public DefaultDiscountHelper(decimal discountParam) {
+            discountSize = discountParam;
+        }
 
         public decimal ApplyDiscount(decimal totalParam) {
-            return (totalParam - (DiscountSize / 100m * totalParam));
+            return (totalParam - (discountSize / 100m * totalParam));
         }
     }
 }
