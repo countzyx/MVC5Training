b20e4e0222c0f9a1526fcb5520ae0d2c4997b28d
Pedro R. Jiménez <ptpedroj@gmail.com>
2014-09-12 01:33:31 -0400

    Pro ASP.Net MVC 5 Platform - Ch 13 - Continuing the Setup
    Continued setting up the foundation. Created the user manager and made an
    Owin config in a non-standard way - by putting an IdentityConfig class in
    App_Start, setting the namespace to just Users, and setting the web.config
    appSetting "owin:AppStartup" to "Users.IdentityConfig" (from the last checkin).

---
+++ Users/Users/Infrastructure/AppUserManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+using Users.Models;
+
+namespace Users.Infrastructure {
+    public class AppUserManager : UserManager<AppUser> {
+        public AppUserManager(IUserStore<AppUser> store) : base(store) { }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context) {
+            var db = context.Get<AppIdentityDbContext>();
+            var manager = new AppUserManager(new UserStore<AppUser>(db));
+
+            return manager;
+        }
+    }
+} 
