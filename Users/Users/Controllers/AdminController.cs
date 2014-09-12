b71e0f57db5f58bb9913b31b1a74f643811f5ac0
Pedro R. Jiménez <ptpedroj@gmail.com>
2014-09-12 02:18:45 -0400

    Pro ASP.Net MVC 5 Platform - Ch 13 - Initial Admin Controllers
    Fixed a configuration issue because I created the project with .Net 4.5 and
    then changed it to .Net 4.5.1. I needed to update all the NuGet packages. It
    was showing errors in the views, as if Visual Studio had trouble resolving
    some classes for Intellisense.
    
    Created an Admin controller and a view that enumerates users. Now need to add
    the rest of the CRUD.

---
+++ Users/Users/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+using System.Web.Mvc;
+
+using Users.Infrastructure;
+
+namespace Users.Controllers {
+    public class AdminController : Controller {
+        public ActionResult Index() {
+            return View(UserManager.Users);
+        }
+
+        public AppUserManager UserManager {
+            get {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+    }
+}
