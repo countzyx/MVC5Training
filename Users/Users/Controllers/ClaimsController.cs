f83148ae62378494d6ef26eb2f6d680b64bdc15b
Pedro R. Jiménez <ptpedroj@gmail.com>
2014-10-14 19:36:57 -0400

    Pro ASP.Net MVC 5 Platform - Ch 15 - Started Using Claims
    Added a new ClaimsController with a view, etc. to show all of the claims that
    are part of the logged in user's identity.

---
+++ Users/Users/Controllers/ClaimsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Users.Controllers
+{
+    public class ClaimsController : Controller {
+        [Authorize]
+        public ActionResult Index() {
+            var id = HttpContext.User.Identity as ClaimsIdentity;
+            if (id == null) {
+                return View("Error", new string[] { "No claims available" });
+            } else {
+                return View(id.Claims);
+            }
+        }
+    }
+}
--- Users/Users/Infrastructure/IdentityHelpers.cs
+++ Users/Users/Infrastructure/IdentityHelpers.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -7,6 +11,17 @@ namespace Users.Infrastructure {
         public static MvcHtmlString GetUserName(this HtmlHelper html, string id) {
             var mgr = HttpContext.Current.GetOwinContext().GetUserManager<AppUserManager>();
             return new MvcHtmlString(mgr.FindByIdAsync(id).Result.UserName);
+        }
+
+        public static MvcHtmlString ClaimType(this HtmlHelper html, string claimType) {
+            var fields = typeof(ClaimTypes).GetFields();
+            foreach (var field in fields) {
+                if (field.GetValue(null).ToString() == claimType) {
+                    return new MvcHtmlString(field.Name);
+                }
+            }
+
+            return new MvcHtmlString(String.Format("{0}", claimType.Split('/', '.').Last()));
         }
     }
 }
