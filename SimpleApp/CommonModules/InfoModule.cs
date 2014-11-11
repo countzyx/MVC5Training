4c597f2197fccd5090c241cfacb0ab8809d0371a
Pedro R. Jiménez <countzyx@gmail.com>
2014-11-11 17:19:20 -0500

    Pro ASP.Net MVC 5 Platform - Ch 04 - Created a Module Class Library
    By keeping modules in a separate library -
    1. The library should be more reusable;
    2. The modules registration can be automated;
    3. The app as a whole can be more testable.

---
+++ SimpleApp/CommonModules/InfoModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+
+namespace CommonModules {
+    public class InfoModule : IHttpModule {
+        public void Init(HttpApplication app) {
+            app.EndRequest += (src, args) => {
+                var ctx = HttpContext.Current;
+                ctx.Response.Write(String.Format("<div class='alert alert-success'>URL: {0} Status: {1}</div>",
+                    ctx.Request.RawUrl, ctx.Response.StatusCode));
+            };
+        }
+
+        public void Dispose() { }
+    }
+}
