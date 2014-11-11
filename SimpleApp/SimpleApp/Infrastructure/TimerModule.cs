e73ec8fcc9c259a5771b5c655302290f9a1a105a
Pedro R. Jiménez <countzyx@gmail.com>
2014-11-11 16:54:17 -0500

    Pro ASP.Net MVC 5 Platform - Ch 04 - Created an In-App Module
    Skipped Chapter 03; it was too simplistic for me to bother with it. Created a
    module that times the difference between the beginning and the end of the
    request.

---
+++ SimpleApp/SimpleApp/Infrastructure/TimerModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace SimpleApp.Infrastructure {
+    public class TimerModule : IHttpModule {
+        private Stopwatch timer;
+
+        public void Init(HttpApplication app) {
+            app.BeginRequest += OnBeginRequest;
+            app.EndRequest += OnEndRequest;
+        }
+
+        public void OnBeginRequest(object src, EventArgs args) {
+            HttpContext ctx = HttpContext.Current;
+            timer = Stopwatch.StartNew();
+        }
+
+        public void OnEndRequest(object src, EventArgs args) {
+            HttpContext ctx = HttpContext.Current;
+            ctx.Response.Write(String.Format("<div class='alert alert-success'>Elapsed: {0:F5} seconds</div>",
+                ((float)timer.ElapsedTicks / Stopwatch.Frequency)));
+        }
+
+        public void Dispose() { }
+    }
+}
